59eeb149750253665304834fde3216ad7c5ce4e1
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "eventgrid_partner_registrations" {
   description = "All eventgrid_partner_registration resources"
   value       = azurerm_eventgrid_partner_registration.eventgrid_partner_registrations
 }
+output "eventgrid_partner_registrations_name" {
+  description = "List of name values across all eventgrid_partner_registrations"
+  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.name]
+}
+output "eventgrid_partner_registrations_partner_registration_id" {
+  description = "List of partner_registration_id values across all eventgrid_partner_registrations"
+  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.partner_registration_id]
+}
+output "eventgrid_partner_registrations_resource_group_name" {
+  description = "List of resource_group_name values across all eventgrid_partner_registrations"
+  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.resource_group_name]
+}
+output "eventgrid_partner_registrations_tags" {
+  description = "List of tags values across all eventgrid_partner_registrations"
+  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.tags]
+}
 
