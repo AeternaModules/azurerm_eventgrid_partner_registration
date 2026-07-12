05faaba6112f900d6897663b3eec56b191d9e7d3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "eventgrid_partner_registrations" {
-  description = "All eventgrid_partner_registration resources"
-  value       = azurerm_eventgrid_partner_registration.eventgrid_partner_registrations
-}
 output "eventgrid_partner_registrations_name" {
-  description = "List of name values across all eventgrid_partner_registrations"
-  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.name]
+  description = "Map of name values across all eventgrid_partner_registrations, keyed the same as var.eventgrid_partner_registrations"
+  value       = { for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : k => v.name }
 }
 output "eventgrid_partner_registrations_partner_registration_id" {
-  description = "List of partner_registration_id values across all eventgrid_partner_registrations"
-  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.partner_registration_id]
+  description = "Map of partner_registration_id values across all eventgrid_partner_registrations, keyed the same as var.eventgrid_partner_registrations"
+  value       = { for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : k => v.partner_registration_id }
 }
 output "eventgrid_partner_registrations_resource_group_name" {
-  description = "List of resource_group_name values across all eventgrid_partner_registrations"
-  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventgrid_partner_registrations, keyed the same as var.eventgrid_partner_registrations"
+  value       = { for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : k => v.resource_group_name }
 }
 output "eventgrid_partner_registrations_tags" {
-  description = "List of tags values across all eventgrid_partner_registrations"
-  value       = [for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : v.tags]
+  description = "Map of tags values across all eventgrid_partner_registrations, keyed the same as var.eventgrid_partner_registrations"
+  value       = { for k, v in azurerm_eventgrid_partner_registration.eventgrid_partner_registrations : k => v.tags }
 }
 
